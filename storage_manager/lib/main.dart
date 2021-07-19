15a4ded1169dd9343649c177d85ea1dc351d6c8b
LachyP <lachiep@live.com.au>
2021-07-19 14:56:58 +0930

    added in bottom navigation bar and basic layout, ready for feature development to begin

--- storage_manager/lib/main.dart
+++ storage_manager/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -46,19 +46,72 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  //int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
+//  void _incrementCounter() {
+  //  setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+    //  _counter++;
+   // });
+  //}
+  int _selectedPage = 0;
+  final List<Widget> pageList = [
+    LandingPage(),
+    SearchPage(),
+    ScanPage(),
+    NewCratePage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Storage Manager'),
+      ),
+      body: IndexedStack(
+        index: _selectedPage,
+        children: pageList,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text('Search By Item'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_alt_outlined),
+            title: Text('Scan Crate Code'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.create),
+            title: Text('Add New Crate'),
+          )
+        ],
+          currentIndex: _selectedPage,
+          selectedItemColor: Colors.black,
+          onTap: _onItemTapped,
+      )
+    );
+  }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedPage = index;
     });
   }
 
+
+
+
+  /*
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -108,6 +161,77 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+   */
+}
+class LandingPage extends StatefulWidget {
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context){
+    //super.build(context);
+
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'home page',
+        ),
+      ),
+    );
+  }
+}
+
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'search page',
+        ),
+      ),
+    );
+  }
+}
+
+class ScanPage extends StatefulWidget {
+  @override
+  _ScanPageState createState() => _ScanPageState();
+}
+
+class _ScanPageState extends State<ScanPage> {
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'scan page',
+        ),
+      ),
+    );
+  }
+}
+
+class NewCratePage extends StatefulWidget {
+  @override
+  _NewCratePageState createState() => _NewCratePageState();
+}
+
+class _NewCratePageState extends State<NewCratePage> {
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'new crate',
+        ),
+      ),
     );
   }
 }
